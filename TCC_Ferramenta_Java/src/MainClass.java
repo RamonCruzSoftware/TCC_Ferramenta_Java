3586d935d87cb0895acefbe85bebe33a6e233f74
kverrna <alissonnunes@Ramons-MacBook-Pro-2.local>
2014-08-27 22:09:58 -0300

    commit inicial

---
+++ TCC_Ferramenta_Java/src/MainClass.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JOptionPane.showMessageDialog(null, "Hello Mundo");
+	}
+
+}
