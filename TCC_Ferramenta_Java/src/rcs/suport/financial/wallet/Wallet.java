189166a2aeb802e0feaa07bfccdd39150fced5c4
kverrna <kverrna@gmail.com>
2014-10-02 12:25:44 -0300

    Gets e Sets

--- TCC_Ferramenta_Java/src/rcs/suport/financial/wallet/Wallet.java
+++ TCC_Ferramenta_Java/src/rcs/suport/financial/wallet/Wallet.java
@@ -6,15 +6,87 @@ import java.util.ArrayList;
 public class Wallet 
 {
 	
-	String userID;
-	double walletValue;
-	double walletRisck;
-	double wallterPercent;
+	private String userID;
+	private double walletValue;
+	private double walletRisck;
+	private double wallterPercent;
 	
-	ArrayList<Stock> stocksList;
+	private ArrayList<Stock> stocksList;
+	
+	
 	
 	
 	public Wallet(){}
+
+
+
+
+	public String getUserID() {
+		return userID;
+	}
+
+
+
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+
+
+
+	public double getWalletValue() {
+		return walletValue;
+	}
+
+
+
+
+	public void setWalletValue(double walletValue) {
+		this.walletValue = walletValue;
+	}
+
+
+
+
+	public double getWalletRisck() {
+		return walletRisck;
+	}
+
+
+
+
+	public void setWalletRisck(double walletRisck) {
+		this.walletRisck = walletRisck;
+	}
+
+
+
+
+	public double getWallterPercent() {
+		return wallterPercent;
+	}
+
+
+
+
+	public void setWallterPercent(double wallterPercent) {
+		this.wallterPercent = wallterPercent;
+	}
+
+
+
+
+	public ArrayList<Stock> getStocksList() {
+		return stocksList;
+	}
+
+
+
+
+	public void setStocksList(ArrayList<Stock> stocksList) {
+		this.stocksList = stocksList;
+	}
 	
 	
 	
