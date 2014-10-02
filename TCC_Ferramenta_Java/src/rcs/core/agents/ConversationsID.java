ea28bddf119fc4e738f7f4fc3cc5fa084219310f
kverrna <kverrna@gmail.com>
2014-10-02 11:29:39 -0300

    classe com atributos estaticos para padronizar as conversas entre agentes

---
+++ TCC_Ferramenta_Java/src/rcs/core/agents/ConversationsID.java
@@ -0,0 +1,18 @@
+package rcs.core.agents;
+
+public class ConversationsID {
+	
+	//Ontologies 
+	public static final String ONTOLOGY_CREATION="creation";
+	
+	//Language
+	public static final String LANGUAGE="English";
+	
+	
+	//Ids
+	public static final String CREATE_MANAGER="Are you Ok?";
+	public static final String REQUEST_VALUES_EXPERTS="Stokes values, please.";
+	public static final String INIT_WORK_EXPERTS="init work";
+	
+
+}
