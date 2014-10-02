43468c01e80461c35a02957ccc39dfd3c3b7fe5e
kverrna <kverrna@gmail.com>
2014-10-02 12:25:32 -0300

    Gets e Sets

--- TCC_Ferramenta_Java/src/rcs/suport/financial/wallet/Stock.java
+++ TCC_Ferramenta_Java/src/rcs/suport/financial/wallet/Stock.java
@@ -7,17 +7,81 @@ import rcs.suport.financial.partternsCandleStick.CandleStick;
 
 public class Stock {
 
-	 String codeName;
-	 String type;
-	 String companyShortName;
-	 String sector;
-	 Map<Date,CandleStick> candleSticks;
-	 Map<Date, Double> volumes;
+	 private String codeName;
+	 private String type;
+	 private String companyShortName;
+	 private String sector;
+	 private Map<Date,CandleStick> candleSticks;
+	 private Map<Date, Double> volumes;
 	 
-	 CandleStick currentCandleStick;
-	 Double currentVolume;
+	 private CandleStick currentCandleStick;
+	 private Double currentVolume;
 	 
 	 Stock(){}
+
+	public String getCodeName() {
+		return codeName;
+	}
+
+	public void setCodeName(String codeName) {
+		this.codeName = codeName;
+	}
+
+	 public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCompanyShortName() {
+		return companyShortName;
+	}
+
+	public void setCompanyShortName(String companyShortName) {
+		this.companyShortName = companyShortName;
+	}
+
+	public String getSector() {
+		return sector;
+	}
+
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+
+	public Map<Date,CandleStick> getCandleSticks() {
+		return candleSticks;
+	}
+
+	public void setCandleSticks(Map<Date,CandleStick> candleSticks) {
+		this.candleSticks = candleSticks;
+	}
+
+	Map<Date, Double> getVolumes() {
+		return volumes;
+	}
+
+	void setVolumes(Map<Date, Double> volumes) {
+		this.volumes = volumes;
+	}
+
+	public CandleStick getCurrentCandleStick() {
+		return currentCandleStick;
+	}
+
+	public void setCurrentCandleStick(CandleStick currentCandleStick) {
+		this.currentCandleStick = currentCandleStick;
+	}
+
+	public Double getCurrentVolume() {
+		return currentVolume;
+	}
+
+	public void setCurrentVolume(Double currentVolume) {
+		this.currentVolume = currentVolume;
+	}
 	
 	
 	
