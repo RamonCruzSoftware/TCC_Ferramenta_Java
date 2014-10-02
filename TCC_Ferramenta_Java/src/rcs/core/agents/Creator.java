156267a7dce16879979391c1afb3fe7920180e23
kverrna <kverrna@gmail.com>
2014-10-02 11:30:29 -0300

    Novo agente criado, este ficara responsavel por prover a ponte entre os usuarios e os agentes

--- TCC_Ferramenta_Java/src/rcs/core/agents/Creator.java
+++ TCC_Ferramenta_Java/src/rcs/core/agents/Creator.java
@@ -1,9 +1,9 @@
 package rcs.core.agents;
 
-import java.io.IOException;
-
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
@@ -11,7 +11,9 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.lang.acl.ACLMessage;
 import jade.wrapper.AgentController;
 import jade.wrapper.PlatformController;
-import rcs.suport.util.database.mongoDB.JadeConnectToMongoTest;
+
+import java.io.IOException;
+
 import rcs.suport.util.database.mongoDB.dao.OrdersCreateDao;
 import rcs.suport.util.database.mongoDB.pojo.OrdersCreate;
 
@@ -38,14 +40,13 @@ public class Creator  extends Agent{
 			System.out.println("I'm live... My name is "+this.getLocalName());
 			
 			
-			new JadeConnectToMongoTest();
+		//	new JadeConnectToMongoTest(); //Nao lembro o pq dessa linha
 		
-			addBehaviour(new TickerBehaviour(this,10) {
-				
-				
-				/**
-				 * 
-				 */
+			/**
+			 * This Behaviour will go create a Manager Agent for each users
+			 */
+			addBehaviour(new TickerBehaviour(this,10) 
+			{
 				private static final long serialVersionUID = 1L;
 
 				@Override
@@ -60,6 +61,9 @@ public class Creator  extends Agent{
 					}	
 				}
 			});
+			
+			
+			
 		}catch(Exception e)
 		{
 			e.printStackTrace();
@@ -82,11 +86,21 @@ public class Creator  extends Agent{
 			e.printStackTrace();
 		}
 	}
-	//Methods for manager the manager team
-	public void createManagerForUser(final String nameAgentManager,String userPerfilType,double userValue)
+	
+	
+	
+	/**
+	 * This method create a team for each users.
+	 * 
+	 * @param nameAgentManager : It's a manager's name formated for Manager_+ <Use-login> 
+	 * @param userPerfilType : It's the profile type of user. Can be "Conservador","Moderado" and "Corajoso"
+	 * @param userValue : It's the user's value for investiments
+	 */
+	public void createManagerForUser(final String nameAgentManager,int userPerfilType,double userValue)
 	{
 		PlatformController container=getContainerController();
-		try {
+		try 
+		{
 			AgentController  agentController=container.createNewAgent(nameAgentManager, "rcs.core.agents.Manager", null);
 			agentController.start();
 			
@@ -95,27 +109,69 @@ public class Creator  extends Agent{
 		{
 			e.printStackTrace();
 		}
+		
+		
+
+			
+			
+			addBehaviour(new Behaviour(this) 
+			{
+				boolean stopBehaviour=false;
+				
+				@Override
+				public boolean done() 
+				{
+					return stopBehaviour;
+				}
+				
+				@Override
+				public void action() {
+					//Send Message to New Manager
+					ACLMessage message=new ACLMessage(ACLMessage.INFORM);
+					message.addReceiver(new AID(nameAgentManager,AID.ISLOCALNAME));
+					message.setLanguage(ConversationsID.LANGUAGE);
+					message.setOntology(ConversationsID.ONTOLOGY_CREATION);
+					message.setConversationId(ConversationsID.CREATE_MANAGER);
+					message.setContent(ConversationsID.CREATE_MANAGER);
+					
+					System.out.println("Creater says for "+nameAgentManager+": "+message.getContent());
+					myAgent.send(message);
+					//Listener messages 
+					try
+					{
+						ACLMessage messages=myAgent.receive();
+						if(messages!=null)
+						{
+							if(message.getConversationId()==ConversationsID.CREATE_MANAGER)
+							{
+								System.out.println("Manager replies: "+message.getContent());
+								stopBehaviour=true;
+							}
+						}else block();
+						
+					}catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+					
+				}
+			});
+			
 			addBehaviour(new OneShotBehaviour() {
 				
-				/**
-				 * 
-				 */
 				private static final long serialVersionUID = 1L;
 
 				@Override
 				public void action() {
-					
-					
 					try {
 						
 						ACLMessage message = new ACLMessage(ACLMessage.INFORM);
 						message.addReceiver(new AID(nameAgentManager,AID.ISLOCALNAME));
-						message.setLanguage("English");
-						message.setOntology("create");
+						message.setLanguage(ConversationsID.LANGUAGE);
+						message.setOntology(ConversationsID.ONTOLOGY_CREATION);
+						message.setConversationId(ConversationsID.INIT_WORK_EXPERTS);
 						message.setContentObject(newOrderCreate);
 						myAgent.send(message);
-						
-						
 					
 					} catch (IOException e) {
 						e.printStackTrace();
@@ -123,9 +179,55 @@ public class Creator  extends Agent{
 	
 					
 				}
-			});	
+			});
+			
+			
+			/*
+			addBehaviour(new CyclicBehaviour(this) 
+			{
+				
+				
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void action() {
+					
+					//Send Message to New Manager
+					ACLMessage message=new ACLMessage(ACLMessage.INFORM);
+					message.addReceiver(new AID(nameAgentManager,AID.ISLOCALNAME));
+					message.setLanguage(ConversationsID.LANGUAGE);
+					message.setOntology(ConversationsID.ONTOLOGY_CREATION);
+					message.setConversationId(ConversationsID.CREATE_MANAGER);
+					message.setContent(ConversationsID.CREATE_MANAGER);
+					
+					myAgent.send(message);
+					//Listener messages 
+					try
+					{
+						ACLMessage messages=myAgent.receive();
+						if(messages!=null)
+						{
+							if(message.getConversationId()==ConversationsID.CREATE_MANAGER)
+							{
+								System.out.print("Manager response: "+message.getContent());
+							}
+						}else block();
+						
+					}catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+
+				}
+				
+				
+			});
+			
+			
+		*/	
 		
 	}
+	
 	public void droManagerForUser(String userIdentifier)
 	{
 		
