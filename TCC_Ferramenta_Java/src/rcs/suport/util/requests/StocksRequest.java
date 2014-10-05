0ababdb9bbc988459031fe76f8a324ad7d875617
kverrna <kverrna@gmail.com>
2014-10-05 17:13:21 -0300

    Requisicoes direto com o site Yahoo financas implementado

--- TCC_Ferramenta_Java/src/rcs/suport/util/requests/StocksRequest.java
+++ TCC_Ferramenta_Java/src/rcs/suport/util/requests/StocksRequest.java
@@ -1,5 +1,14 @@
 package rcs.suport.util.requests;
 
+import java.util.ArrayList;
+
+import rcs.suport.financial.partternsCandleStick.CandleStick;
+
 public interface StocksRequest {
+	
+	public CandleStick getCurrentValue(String stockCodeName);
+	public ArrayList<CandleStick> getHistoryValue(String stockCodeName);
+	
+	
 
 }
